34a1c650904bd95519a1ee463c78637f73a783be
Amir Esmaeili <amir79esmaeili@gmail.com>
2023-01-02 23:26:35 +0330

    movie repository added

---
+++ repository/movie_repository.py
@@ -0,0 +1,22 @@
+from dummy_repository import DummyRepository
+from repository_interface import RepositoryInterface
+
+
+class MovieRepository(RepositoryInterface, DummyRepository):
+    def config(self, configuration_str: str):
+        DummyRepository.config(self, configuration_str)
+
+    def get(self, pk: int):
+        pass
+
+    def create(self, movie):
+        pass
+
+    def update(self, movie):
+        pass
+
+    def list(self, **kwargs):
+        pass
+
+    def delete(self, pk: int):
+        pass
