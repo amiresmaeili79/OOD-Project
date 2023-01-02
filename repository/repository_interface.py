4ab4756eab4b43efa7a6517dad39140486749afd
Amir Esmaeili <amir79esmaeili@gmail.com>
2023-01-02 22:15:45 +0330

    repository interface created

---
+++ repository/repository_interface.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+
+class RepositoryInterface(ABC):
+    """
+    Interface for respositories
+    """
+
+    @abstractmethod
+    def get(**kwargs):
+        raise NotImplementedError("Implement Me")
+
+    @abstractmethod
+    def create(**kwargs):
+        raise NotImplementedError("Implement Me")
+
+    @abstractmethod
+    def update(**kwargs):
+        raise NotImplementedError("Implement Me")
+
+    @abstractmethod
+    def list(**kwargs):
+        raise NotImplementedError("Implement Me")
+
+    @abstractmethod
+    def delete(**kwargs):
+        raise NotImplementedError("Implement Me")
