f2865d4a6e95ce37654705f71d2ed9ff884080fa
Niki Nezakati <59639193+nikinezakati@users.noreply.github.com>
2023-01-02 23:34:49 +0330

    Update and rename Actor.py to actor.py

--- Domain/actor.py
+++ Domain/actor.py
@@ -7,19 +7,19 @@ class Actor:
         self.name = name
         self.age = 0
 
-    def getName(self):
+    def get_name(self):
         #code
         return True     
 
-    def setName(self,name):
+    def set_name(self,name):
         #code
         return True     
 
-    def getAge(self):
+    def get_age(self):
         #code
         return True  
 
-    def setAge(self,age):
+    def set_age(self,age):
         #code
         return True      
 
