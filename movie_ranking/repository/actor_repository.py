847a56f73b9037573fadf8450db82cd1d31a3c68
Amir Esmaeili <amir79esmaeili@gmail.com>
2023-01-03 00:02:59 +0330

    Updated folder structure

--- movie_ranking/repository/actor_repository.py
+++ movie_ranking/repository/actor_repository.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from dummy_repository import DummyRepository
+from movie_ranking.domain.movie import Movie
 from repository_interface import RepositoryInterface
 
 
@@ -6,16 +9,16 @@ class ActorRepository(RepositoryInterface, DummyRepository):
     def config(self, configuration_str: str):
         DummyRepository.config(self, configuration_str)
 
-    def get(self, pk: int):
+    def get(self, pk: int) -> Movie:
         pass
 
-    def create(self, movie):
+    def create(self, movie: Movie) -> Movie:
         pass
 
-    def update(self, movie):
+    def update(self, movie: Movie) -> Movie:
         pass
 
-    def list(self, **kwargs):
+    def list(self, **kwargs) -> List[Movie]:
         pass
 
     def delete(self, pk: int):
