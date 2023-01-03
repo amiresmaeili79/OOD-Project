85ca5e387c75e845f8e7248a68952001136d82a4
parsaeissazadeh <parsa.eissazadeh@snapp.cab>
2023-01-03 22:22:42 +0330

    use abs

--- service_layer/isearch.py
+++ service_layer/isearch.py
@@ -1,6 +1,9 @@
+from abc import ABC, abstractmethod
 from movie_ranking.domain.movie import  Movie
 
 
-class SearchInterface:
+class SearchInterface(ABC):
+
+    @abstractmethod
     def search(self, name) -> [Movie]:
         pass
