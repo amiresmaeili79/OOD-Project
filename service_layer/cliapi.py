f78a6e4660b2d5248d5e2f5abeb08d3a54fec866
parsaeissazadeh <parsa.eissazadeh@snapp.cab>
2023-01-03 22:17:16 +0330

    define cliapi methods

---
+++ service_layer/cliapi.py
@@ -0,0 +1,21 @@
+from movie_ranking.domain.movie import  Movie
+
+class CliApi:
+
+    def __init__(self):
+        pass
+        
+    def search(self, name, type) -> [Movie]:
+        pass
+
+    def create_movie(self) -> Movie:
+        pass
+
+    def update_movie(self) -> Movie:
+        pass
+
+    def get_ranking(self) -> [Movie]:
+        pass
+
+    def udpate_ranking(self):
+        pass
